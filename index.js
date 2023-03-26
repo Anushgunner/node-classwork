e6a6d052f12cc8deb0a14d6b7e6408d60294bc95
Gothic mist <anushgunner@gmail.com>
2023-03-26 08:44:50 +0545

    gggsdfsdf

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+//import json data
+const quotesData = require("./quotes.json")
+
+
+const getRandomIndex = () => {
+    return Math.floor(Math.random() * (quotesData.quotes.length))
+}
+
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Quotes app")
+})
+
+
+app.get("/quote", (req, res) => {
+    res.json(quotesData.quotes[getRandomIndex()])
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`)
+})
+
+
+
+console.log("change for the 2nd commit")
+
+// hghj
